de9ac0c7ff08455f0dbc8a6911f735f0ce61a898
KANEKO Michihito <mikaneko@jmdc.co.jp>
2022-07-17 13:08:16 +0900

    useEffectを使用

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Article from './components/Article';
 import TextInput from './components/TextInput';
 import Counter from './components/Counter';
+import ToggleButton from './components/ToggleButton';
 
 function App() {
   return (
     <div className="App">
       <TextInput />
       <Counter />
+      <ToggleButton />
       <Article
       title={'日本一わかりやすい焼きそば入門'}
       content={'焼きそばは日本で最も美味しい食べ物です。（個人的感想）'}
---
+++ src/components/ToggleButton.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+const ToggleButton = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(prevState => !prevState)
+  }
+  return (
+    <button onClick={toggle}>
+      {open ? 'open' : 'close'}
+    </button>
+  );
+};
+
+export default ToggleButton;
