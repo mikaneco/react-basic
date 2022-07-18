9c4d12330a987bcd1bfb1dbf6615acc27fc18997
KANEKO Michihito <mikaneko@jmdc.co.jp>
2022-07-18 23:38:11 +0900

    クリーンナップを使用

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import userEvent from '@testing-library/user-event';
+import React, {useEffect, useState} from 'react';
 
 const ToggleButton = () => {
   const [open, setOpen] = useState(false);
@@ -6,6 +7,16 @@ const ToggleButton = () => {
   const toggle = () => {
     setOpen(prevState => !prevState)
   }
+
+  useEffect(() => {
+    console.log('current state is', open)
+    if(open){
+      console.log('subscribe database...')
+    }
+    return () => {
+      console.log('unsubscribe database...')
+    }
+  })
   return (
     <button onClick={toggle}>
       {open ? 'open' : 'close'}
