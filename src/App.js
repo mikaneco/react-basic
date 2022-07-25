e6cb7a6f9c90f8cf5b3eed63fa661f379d0b7267
KANEKO Michihito <mikaneko@jmdc.co.jp>
2022-07-25 22:51:51 +0900

    API取得結果を画面に出力

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [id, setId] = useState('MichiKaneko');
+  const [name, setName] = useState('')
+  const ids = ['MichiKaneko', 'aws', 'google', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
 
   useEffect(() => {
     fetch(`https://api.github.com/users/${id}`)
     .then(res => res.json())
     .then(data => {
       console.log(data)
+      setName(data.name);
     })
     .catch(error => {
       console.error(error)
@@ -30,6 +37,10 @@ function App() {
       title={'日本一わかりやすい焼きそば入門'}
       content={'焼きそばは日本で最も美味しい食べ物です。（個人的感想）'}
       />
+      <div>
+        <p>{id}のGitHub上の名前は{name}です。</p>
+        <button onClick={getRandomId}>IDを変更</button>
+      </div>
     </div>
   ); 
 }
